07b9f9ac7d7d561ab1e7d113051a5dfef5a2c94d
hrp-ELEMENTAR <hrp@elementar.site>
2026-01-09 13:48:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import PoliticaPrivacidade from "./pages/PoliticaPrivacidade";
+
+import AreaCliente from "./pages/AreaCliente";
+import AreaRegulador from "./pages/AreaRegulador";
 import Login from "./pages/Login";
 
 const queryClient = new QueryClient();
@@ -18,8 +22,17 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+
+          {/* páginas */}
           <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
-          <Route path="/login" element={<Login />} />
+
+          {/* áreas */}
+          <Route path="/area-cliente" element={<AreaCliente />} />
+          <Route path="/area-regulador" element={<AreaRegulador />} />
+
+          {/* login por tipo */}
+          <Route path="/login/:tipo" element={<Login />} />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
